7bbb8840ca332850d8e232cf1cffabafe64ba2e0
VARUN-009 <varunpunia009@gmail.com>
2024-08-01 19:09:26 +0530

    Final Commit

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Peter the Penguin",
+    job: "Software Engineer",
+    image: "https://t4.ftcdn.net/jpg/05/65/72/25/360_F_565722576_O1ukHVh1pIn2D3OS2grnG9w2RwwykPXf.jpg",
+    text: "Hello! I’m Peter the Penguin and as a penguin, I've always been drawn to the precision and elegance of nature, and I bring that same passion to my work as a software engineer. At ArcticTech Innovations, I’ve had the unique opportunity to blend my love for technology with my natural problem-solving instincts.",
+  },
+  {
+    id: 2,
+    name: "Ela the Elephant",
+    job: "Graphic Designer",
+    image: "https://t3.ftcdn.net/jpg/07/37/16/14/360_F_737161452_oOt3F4JnOq2YzUqCX2NgKul7sfFrpqAp.jpg",
+    text: "Hello! I’m Ella the Elephant, and I’m here to trumpet my experience as a graphic designer. With my impressive memory and attention to detail, I can recall and apply the finest design elements with grace and precision. Just like an elephant never forgets, I ensure that every design is memorable and impactful.",
+  },
+  {
+    id: 3,
+    name: "Charlie the Chimpanzee",
+    job: "Marketing Manager",
+    image: "https://t3.ftcdn.net/jpg/07/87/85/02/240_F_787850291_WhxfeT2VVMKevKBkDtZVyYBWWmne5X1O.jpg",
+    text: "Hey there! I’m Charlie the Chimpanzee, and I’m swinging by to share my enthusiasm for marketing management. With my boundless energy and clever problem-solving skills, I’m adept at developing marketing strategies that really resonate with your audience. Just like a chimp in the jungle, I’m always on the move, adapting and optimizing to keep things fresh and engaging.",
+  },
+  {
+    id: 4,
+    name: "Gerry the Giraffe",
+    job: "Content Strategist",
+    image: "https://t3.ftcdn.net/jpg/08/12/90/54/240_F_812905487_8IOkCUa4XV34D2riWWpPhhBagFz1PQTF.jpg",
+    text: "Hello there! I’m Gerry the Giraffe, and I’m reaching out from the top branches of the tallest trees to share a bit about my experience in content strategy. With my long neck and even longer view, I’ve got a unique perspective on crafting content that stands head and shoulders above the rest!",
+  },
+  {
+    id: 5,
+    name: "Harry the Horse",
+    job: "Data Analyst",
+    image: "https://t3.ftcdn.net/jpg/08/63/10/84/240_F_863108407_UN0hfiG0pTDHNjgBjRq2aXv9On3eE4Y7.jpg",
+    text: "Giddy up! I’m Harry the Horse, and I’m here to trot you through my journey as a data analyst. With my keen sense of observation and thorough attention to detail, I can gallop through data sets and uncover insights with precision. Just like a well-tuned racehorse, I make sure that every data point is analyzed efficiently and effectively.",
+  },
+];
+
+export default reviews;
